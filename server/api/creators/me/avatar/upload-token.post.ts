b9b8cde24f0a040e6db571630ceaecb6869b92b5
iyanuloluwa-Miracle <iyanudina@gmail.com>
2026-09-10 06:03:02 +0100

    fixes: deployment bug fix

--- server/api/creators/me/avatar/upload-token.post.ts
+++ server/api/creators/me/avatar/upload-token.post.ts
@@ -1,7 +1,7 @@
-import { requireUser } from '../../../lib/auth';
-import { defineApiHandler } from '../../../lib/define-api';
-import { assertRateLimit } from '../../../lib/rate-limit';
-import { createAvatarUploadToken } from '../../../services/byteship/byteship.service';
+import { requireUser } from '../../../../lib/auth';
+import { defineApiHandler } from '../../../../lib/define-api';
+import { assertRateLimit } from '../../../../lib/rate-limit';
+import { createAvatarUploadToken } from '../../../../services/byteship/byteship.service';
 
 export default defineApiHandler(async (event) => {
   const user = await requireUser(event);
